41377a0349b945d236ec1116a03978542b00f61f
nyagetiria <nyagetirianaomi@gmail.com>
2025-06-28 23:07:41 +0300

    index.js

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (let num of array) {
+    const complement = target - num;
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+    seenNumbers.add(num);
+  }
+
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Big O time complexity: O(n)
+  - We loop through the array once.
+  - Set operations (add, has) are O(1).
 */
 
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  - Create a Set to store numbers we've seen
+  - For each number in the array:
+    - Calculate the complement (target - current number)
+    - If the complement is in the Set, return true
+    - Otherwise, add the current number to the Set
+  - If no matches found, return false
 */
 
 /*
-  Add written explanation of your solution here
+  Explanation:
+  We're trying to find two numbers in the array that add up to the target.
+  Instead of checking every pair (which would be slow), we use a Set to remember
+  the numbers we've seen and check if the number we need (target - current)
+  is already in the Set.
 */
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
